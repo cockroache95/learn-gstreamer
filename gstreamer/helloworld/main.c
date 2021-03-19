3ed6f8cae81aef835809679cd0b5fb30020efce0
Nguyễn Tiến Dũng (VSM-VNCCMR-PCNND) <v.DungNT94@vinsmart.net.vn>
2021-03-19 16:31:56 +0700

    hello world

---
+++ gstreamer/helloworld/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <gst/gst.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Invalid arguments\n");
+        return -1;
+    }
+    printf("Run Helloworld program \nArgument: %s\n",argv[1]);
+              
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+
+    /* Initialize GStreamer */
+    gst_init(&argc, &argv);
+
+    /* Build the pipeline */
+    pipeline =
+        gst_parse_launch("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+                         NULL);
+
+    /* Start playing */
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    /* Wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg =
+        gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                   GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+    /* Free resources */
+    if (msg != NULL)
+        gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+    return 0;
+}
